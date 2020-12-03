3aa7be6538ab7e4de8ddae0531e9148eda376682
area123 <kjs0713kjs@naver.com>
2020-12-03 16:34:40 +0900

    2-1

---
+++ src/main/java/chap02/Criterion.java
@@ -0,0 +1,31 @@
+package chap02;
+
+import chap01.Scoreable;
+
+public class Criterion implements Scoreable {
+    private Weight weight;
+    private Answer answer;
+    private int score;
+
+    public Criterion(Weight weight, Answer answer) {
+        this.weight = weight;
+        this.answer = answer;
+    }
+
+    public Weight getWeight() {
+        return weight;
+    }
+
+    public Answer getAnswer() {
+        return answer;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public int getScore() {
+        return 0;
+    }
+}
--- src/main/java/chap02/Profile.java
+++ src/main/java/chap02/Profile.java
@@ -7,4 +7,18 @@ public class Profile {
     private Map<String, Answer> answers = new HashMap<>();
     private int score;
     private String name;
+
+    public Profile(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void add(Answer answer) {
+        answers.put(answer.getQuestionText(), answer);
+    }
+
+    public boolean matched(Cri)
 }
